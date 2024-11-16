b13e27f6fd36ddd61402e2e65d928f7784d3e5be
Artem_eg <mr.artem.egorov@yandex.ru>
2024-11-16 00:55:34 +0000

    added gzserver launch params, problems with entity

--- launch/bebop_spawn.launch.py
+++ launch/bebop_spawn.launch.py
@@ -11,8 +11,22 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 import xacro
 
 def generate_launch_description():
-
+    launch_file_dir = os.path.join(get_package_share_directory('bebop_simulator_r2'), 'launch')
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    
+    # Launch configuration variables specific to simulation
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    user_account = LaunchConfiguration('user_account', default='artem')
+    waypoint_filter = LaunchConfiguration('waypoint_filter', default='true')
+    EKFActive = LaunchConfiguration('EKFActive', default='false')
+    csvFilesStoring = LaunchConfiguration('csvFilesStoring', default='false')
+    csvFilesStoringTime = LaunchConfiguration('csvFilesStoringTime', default='60.0')
+
+    world = os.path.join(
+        get_package_share_directory('bebop_simulator_r2'),
+        'worlds',
+        'basic.world'
+    )
 
     pkg_box_car_description = get_package_share_directory('bebop_simulator_r2')
     xacro_file = os.path.join(get_package_share_directory('bebop_simulator_r2'), 'urdf/', 'bebop.urdf.xacro')    
@@ -30,6 +44,7 @@ def generate_launch_description():
     else:
         os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
 
+    # Get the urdf file
     params_model={'radius': '0.9',
             'enable_ground_truth': 'true',
             'enable_odometry_sensor_with_noise': 'false',
@@ -50,36 +65,96 @@ def generate_launch_description():
             'z': '0.31',
             'model': 'bebop'
             }
-    param_launch = {'user_account' :"artem",
-        'waypoint_filter' :"true",
-        'EKFActive' :"false",
-        'csvFilesStoring' :"false",
-        'csvFilesStoringTime':"60.0"}
+
     robot_description_config = xacro.process_file(xacro_file, mappings=params_model)
     robot_desc = robot_description_config.toxml()
 
-    # print(robot_desc)
-    
-    # start_steering_control = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(pkg_box_car_description, 'launch', 'steering_control.launch.py'),
-    #     )
-    # ) 
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
+        ),
+        launch_arguments={'world': world}.items()
+    )
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-        # Node(package='bebop_simulator_r2', executable='position_controller_node', arguments=["--ros-args",param_launch], output='screen',
-        #      parameters=[
-        #         ]),
-        Node(
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
+        )
+    )
+    
+    declare_time_cmd =DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    
+    declare_user_account_cmd =DeclareLaunchArgument(
+        'user_account',
+        default_value='artem',
+        description='Use simulation (Gazebo) clock if true')
+        
+    declare_waypoint_filter_cmd =DeclareLaunchArgument(
+        'waypoint_filter',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true')
+    declare_EKFActive_cmd =DeclareLaunchArgument(
+        'EKFActive',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    declare_csvFilesStoring_cmd =DeclareLaunchArgument(
+        'csvFilesStoring',
+        default_value='false',
+        description='Use simulation (Gazebo) clock if true')
+    declare_csvFilesStoringTime_cmd =DeclareLaunchArgument(
+        'csvFilesStoringTime',
+        default_value='60.0',
+        description='Use simulation (Gazebo) clock if true')
+        
+    robot_state_publisher_cmd = Node(
             package="robot_state_publisher",
             executable="robot_state_publisher",
             name="robot_state_publisher",
             parameters=[{'robot_description': robot_desc}
             ],
-            output='both'),
-        #start_steering_control,
-    ])
+            output='both')
+    param_launch = {'user_account' :"artem",
+        'waypoint_filter' :"true",
+        'EKFActive' :"false",
+        'csvFilesStoring' :"false",
+        'csvFilesStoringTime':"60.0"}
+    postion_contrl_cmd =Node(package='bebop_simulator_r2', executable='position_controller_node', arguments=[param_launch], output='screen',
+        parameters=[
+        ])
+    
+    # Launch configuration variables specific to simulation
+    x_pose = LaunchConfiguration('x_pose', default='0.0')
+    y_pose = LaunchConfiguration('y_pose', default='0.0')
+    z_pose = LaunchConfiguration('z_pose', default='0.01')
+
+    start_gazebo_ros_spawner_cmd = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', "robot",
+            '-file', robot_desc,
+            '-x', x_pose,
+            '-y', y_pose,
+            '-z', z_pose
+        ],
+        output='screen',
+    )
+    ld =LaunchDescription()
+    # Declare the launch options
+    ld.add_action(declare_time_cmd)
+    ld.add_action(declare_user_account_cmd)   
+    ld.add_action(declare_waypoint_filter_cmd)
+    ld.add_action(declare_EKFActive_cmd)   
+    ld.add_action(declare_csvFilesStoring_cmd)
+    ld.add_action(declare_csvFilesStoringTime_cmd)   
+    
+    # Add the commands to the launch description
+    ld.add_action(gzserver_cmd)
+    ld.add_action(gzclient_cmd)
+    ld.add_action(robot_state_publisher_cmd)
+    ld.add_action(start_gazebo_ros_spawner_cmd)
+    # ld.add_action(postion_contrl_cmd)
+    return ld
